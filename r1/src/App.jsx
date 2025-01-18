366a58628591045366f3fea0ce8899b8c3ee16df
Navinkumar Singh <singhnavinkumar493@gmail.com>
2025-01-18 23:23:45 +0530

    compo

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Page from './components/Page'
 
 const App = () => {
   return (
-    <h1>
-     navin
-    </h1>
+    <>
+     <Page/>
+    </>
   )
 }
 
---
+++ r1/src/components/Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Home page visible
+    </div>
+  )
+}
+
+export default Page
